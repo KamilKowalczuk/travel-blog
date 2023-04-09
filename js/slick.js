d46f523b275d3ef6fec904afe6bea5bf204752d0
Kamil <kamil.kowalczuk17@gmail.com>
2023-04-09 09:13:55 +0200

    created nav section and half hero

---
+++ js/slick.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$('.header').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2500,
+		arrows: true,
+		mobileFirst: true
+	});
+});
