7a431553d556ae7b9834663e238b444b8ae001f7
Kamil <kamil.kowalczuk17@gmail.com>
2023-04-09 11:35:53 +0200

    hero and nav ended - nav before JS

--- js/slick.js
+++ js/slick.js
@@ -1,10 +1,21 @@
 $(document).ready(function () {
-	$('.header').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 2500,
-		arrows: true,
-		mobileFirst: true
-	});
-});
+    $('.hero__boxes').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4500,
+		pauseOnHover: true,
+        arrows: true,
+		prevArrow: '.hero__prev-btn',
+		nextArrow: '.hero__next-btn',
+        mobileFirst: true,
+        responsive: [{
+            breakpoint: 700,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }],
+
+    });
+})
